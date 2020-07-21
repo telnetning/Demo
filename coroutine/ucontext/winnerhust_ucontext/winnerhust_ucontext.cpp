b9a4552d5b242c54e41de829873497c73b5c567b
telnetning <telnetning@gmail.com>
2020-07-22 00:58:38 +0800

    [coroutine][ucontext][Winnerhust]
    comments winnerhust's ucontext example: uthread.cpp

--- coroutine/ucontext/winnerhust_ucontext/winnerhust_ucontext.cpp
+++ coroutine/ucontext/winnerhust_ucontext/winnerhust_ucontext.cpp
@@ -7,3 +7,89 @@
  * - schedule_finish -> main to determine if all coroutines run out
  */
 
+#include "uthread.h"
+
+int uthread_create(schedule_t &schedule, Fun func, void *arg) {
+    // find FREE location 
+    int id = 0;
+    for (id = 0; i < schedule.max_index; ++i) {
+        if (schedule.threads[i].status == FREE) {
+            break; 
+        } 
+    }
+
+    if (id == schedule.max_index) {
+        schedule.max_index++;
+    }
+
+    uthread_t *t = &(schedule.threads[id]);
+    
+    t->state = RUNNING;
+    t->func = func;
+    t->arg = arg;
+    getcontext(&(t->ctx));
+
+    t->ctx.uc_stack.ss_sp = t->stack;
+    t->ctx.uc_stack.ss_size = DEFAULT_STACK_SIZE;
+    t->ctx.uc_stack.ss_flags = 0;
+    t->ctx.uc_link = &(schedule.main);
+
+    schedule.running_threading = id;
+
+    makecontext(&(t->ctx), (void (*)(void))(uthread_body), 1, &schedule);
+
+    swapcontext(&(schedule.main), &t->ctx); 
+
+    return id;
+}
+
+void uthread_body(schedule_t *ps) 
+{
+    int id = ps->running_threading;
+    if(id != -1) {
+        uthread_t *t = &(ps->threads[id]);  
+        t->func(t->arg);
+        t->state = FREE;
+        ps->running_threading = -1;
+    }
+}
+
+void uthread_resume(schedule_t *schedule, int id) 
+{
+    if(id < 0 || id > schedule.max_index) {
+        return;
+    }
+
+    uthread_t *t = schedule.threads[id];
+    
+    if(t->state == SUSPEND) {
+        swapcontext(&(schedule.main), &(t->ctx)); 
+    }
+}
+
+void thread_yield(schedule_t *schedule) 
+{
+    if(schedule.ruinning_thread != -1) {
+        uthread_t *t = schedule.threads[schedule.running_threading];   
+        t->state = SUSPEND;
+        schedule.running_threading = -1;
+
+        swapcontext(&(t->ctx), &(schedule.main));
+    }          
+
+}
+
+int schedule_finished(const schedule_t *schedule)
+{
+    if(schedule.running_threading != -1) {
+        return 0; 
+    }
+    
+    for(int i = 0;i < schedule.max_index; i++) {
+        if(schedule.threads[i].state != FREE) {
+            return 0; 
+        } 
+    }
+    
+    return 1;
+}
--- coroutine/ucontext/winnerhust_ucontext/winnerhust_ucontext.h
+++ coroutine/ucontext/winnerhust_ucontext/winnerhust_ucontext.h
@@ -1,4 +1,3 @@
-
 /**
  * schedule_t
  *    |
@@ -8,6 +7,17 @@
  */
 
 // Define a coroutine struct
+
+#ifndef MY_UTHREAD_H
+#define MY_UTHREAD_H
+#include <ucontext.h>
+#include <vector>
+
+#define DEFAULT_STACK_SIZE (1024*128)
+#define MAX_UTHREAD_SIZE   1024
+
+enum ThreadState{FREE,RUNNABLE,RUNNING,SUSPEND};
+
 typedef void (*Fun)(void *arg);
 
 typedef struct uthread_t
@@ -16,7 +26,7 @@ typedef struct uthread_t
     Fun func; // coroutine running func
     void *arg; // func arguments
     enum ThreadState state; // FREE,RUNNABLE,RUNNING,SUSPEND
-    char stack[DEFAULT_STACK_SZIE];
+    char stack[DEFAULT_STACK_SIZE];
 } uthread_t;
 
 // Define scheduler struct
@@ -25,11 +35,25 @@ typedef std::vector<uthread_t> Thread_vector;
 typedef struct schedule_t
 {
     ucontext_t main; // main, in many projects, it is called eventloop
-    int running_thread; // the running thread No.
+    int running_threading; // the running thread No.
     Thread_vector threads; 
- 
-    schedule_t():running_thread(-1){} // struct constructor func: this.running_thread = -1
+    int max_index; // current max index
+
+    schedule_t():running_threading(-1), max_index(0){
+        threads = new thread_vector[MAX_UTHREAD_SIZE];       
+        for(int i = 0; i < max_index; i++) {
+            threads[i].state = FREE; 
+        }
+    } // struct constructor func: this.running_threading = -1
+
+    ~schedule_t() {
+        delete [] threads; 
+    }
+
 } schedule_t;
+
+// keep running in schedule.Is it meaningful?
+static void uthread_body(schedule_t *ps);
 
 /**
  * to implement a conroutine lib
@@ -46,3 +70,5 @@ void uthread_yield(schedule_t &schedule);
 void uthread_resume(schedule_t &schedule,int id);
 
 int  schedule_finished(const schedule_t &schedule);
+
+#endif
